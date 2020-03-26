325902143bf3b1431c1d46a7e61a8a9d025d9b3d
Drew Silcock <drew.silcock@stfc.ac.uk>
2020-03-26 15:50:05 +0000

    Fix macro issue with bindgen.

--- build.rs
+++ build.rs
@@ -9,8 +9,24 @@ fn main() {
     println!("cargo:rustc-link-search=native={}/build/QuEST", dst.display());
     println!("cargo:rustc-link-lib=dylib=QuEST");
 
+    // See: https://github.com/rust-lang/rust-bindgen/issues/687
+    let ignored_macros = IgnoreMacros(
+        vec![
+            "FP_INFINITE".into(),
+            "FP_NAN".into(),
+            "FP_NORMAL".into(),
+            "FP_SUBNORMAL".into(),
+            "FP_ZERO".into(),
+            "IPPORT_RESERVED".into(),
+        ]
+        .into_iter()
+        .collect(),
+    );
+
     let bindings = bindgen::Builder::default()
-        .header("QuEST/QuEST/include/QuEST.h")
+        .header("wrapper.h")
+        .parse_callbacks(Box::new(ignored_macros))
+        .rustfmt_bindings(true)
         .generate()
         .expect("Unable to generate bindings");
 
@@ -18,4 +34,17 @@ fn main() {
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
+}
+
+#[derive(Debug)]
+struct IgnoreMacros(HashSet<String>);
+
+impl bindgen::callbacks::ParseCallbacks for IgnoreMacros {
+    fn will_parse_macro(&self, name: &str) -> bindgen::callbacks::MacroParsingBehavior {
+        if self.0.contains(name) {
+            bindgen::callbacks::MacroParsingBehavior::Ignore
+        } else {
+            bindgen::callbacks::MacroParsingBehavior::Default
+        }
+    }
 }
